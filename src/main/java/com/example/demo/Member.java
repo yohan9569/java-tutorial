44d5643826b6646c90f441e55cb519b8b5c2aac5
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-20 21:33:15 +0900

    실습 2-22: @EqualsAndHashCode - equals, hashCode 비교 메서드 생성

--- src/main/java/com/example/demo/Member.java
+++ src/main/java/com/example/demo/Member.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.ToString;
 @Getter // Class 단위
 @Setter
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false) // -> super=Member(1, yohan)
+@EqualsAndHashCode
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
 
@@ -21,18 +23,4 @@ public class Member {
     private String name;
     private int age;
     private String email;
-
-    public boolean equals(Member member) {
-        return this.hashCode() == member.hashCode();
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7; // 소수를 활용
-        hash = 31 * hash + (id == null ? 0 : id.hashCode());
-        hash = 31 * hash + (name == null ? 0 : name.hashCode());
-        hash = 31 * hash + (int) age;
-        hash = 31 * hash + (email == null ? 0 : email.hashCode());
-        return hash;
-    }
 }
