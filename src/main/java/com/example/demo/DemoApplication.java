9b4a93bacdb5d57951dc2e1529455e37cea33607
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-20 20:33:04 +0900

    실습 2-13: 클래스 상속 시 @AllArgsConstructor 사용 불가 + toString 재정의

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.member.Administrator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Member yohan = new Member(1, "yohan", 30, "yohan9569@gmail.com");
+        Administrator yohan = new Administrator(1, "yohan", 30, "yohan9569@gmail.com", "backend");
 
         System.out.println(yohan);            // com.example.demo.Member@527740a2 객체
         System.out.println(yohan.toString()); // com.example.demo.Member@527740a2 객체
---
+++ src/main/java/com/example/demo/member/Administrator.java
@@ -0,0 +1,25 @@
+package com.example.demo.member;
+
+import com.example.demo.Member;
+import java.time.LocalDate;
+
+// @AllArgsConstructor // Lombok needs a default constructor in the base class
+public class Administrator extends Member {
+
+    private String role;
+    private LocalDate allocatedAt;
+
+    public Administrator(Integer id, String name, int age, String email, String role) {
+        super(id, name, age, email);
+        this.role = role;
+        this.allocatedAt = LocalDate.now();
+    }
+
+    @Override // 생략 가능 // 재정의 안하면, 상위 클래스 버전 출력. Member(memberId=1, name=yohan)
+    public String toString() {
+        return String.format(
+            "Administrator(id=%s, name=%s, age=%s, email=%s, role=%s, allocated=%s)",
+            this.getId(), this.getName(), this.getAge(), this.getEmail(), this.role, this.allocatedAt
+        );
+    }
+}
