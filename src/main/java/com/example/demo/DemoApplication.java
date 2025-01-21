67efcbd4afe7db79ee0898b4dd4b9ef03120ca03
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-21 16:43:56 +0900

    실습 2-33: @Value - 불변 객체 VO 필요 기능 한번에. 주의 : final class, @FieldDefaults 추가 및 @Setter 삭제

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,8 +13,8 @@ public class DemoApplication {
 
         System.out.println("---");
         System.out.println(yohan);                // Member(1, yohan)
-        System.out.println(yohan.getName());      // @Getter (@Data = 모든 "FINAL" 필드에 대해 Getter 생성)
-        //yohan.setName("Caron");                 // @Setter (@Data = 모든 "NON-FINAL" 필드에 대해 Setter 생성) - 현재 makeFinal = true
+        System.out.println(yohan.getName());      // @Getter (@Value = 모든 "FINAL" 필드에 대해 Getter 생성)
+        //yohan.setName("Caron");                 // @Setter (@Value = Setter 미생성)
 
         System.out.println("---");
         System.out.println(yohan2);               // Member(1, yohan)
--- src/main/java/com/example/demo/Member.java
+++ src/main/java/com/example/demo/Member.java
@@ -9,16 +9,23 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.Value;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 
-/* @Data
-All together now: A shortcut for
-    @ToString, @EqualsAndHashCode,
-    @Getter on all fields, @Setter on all non-final fields,
-    and @RequiredArgsConstructor!
+/* @Value: Immutable classes made very easy. is the immutable variant of @Data;
+In practice, @Value is shorthand for:
+    final -> class 상속 불가
+    @ToString
+    @EqualsAndHashCode
+    @AllArgsConstructor
+    @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+    @Getter
+다만 관련 메서드의 구현을 명시적으로 포함하면 일부가 생성되지 않고 경고가 표시되지 않습니다. -> 맘에 안 들면 커스텀해서 쓰기.
 */
-@Data // @ALL 이 아닌 @requiredargsconstructor 추가 -> final 붙어야 생성자에 주입 가능. // 아래 설정들도 유효, onlyExplicitlyIncluded 못 넣음 유의
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Value
+@NonFinal // 상속(extends)을 위해서는 @NonFinal 통해 class 에게 자유를
+// @Setter 필드에 @NonFinal 붙여서 setter 만들 수는 있다.
 public class Member {
 
     @ToString.Include(name = "memberId")
@@ -31,5 +38,6 @@ public class Member {
     @EqualsAndHashCode.Exclude
     /*private*/ int age;
     @EqualsAndHashCode.Exclude
+    // @NonFinal
     /*private*/ String email;
 }
