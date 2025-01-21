cd5395fc2229fd8b527ed81bacd5615a40d18975
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-21 14:23:42 +0900

    실습 2-27: @EqualsAndHashCode - 상속시 부모 메서드 호출 (이슈)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Member yohan = new Member(1, "yohan", 10, "yohan@example.com");
-        Member yohan2 = new Member(1, "yohan", 16, "yohan@other.com");
+        Administrator yohan = new Administrator(1, "yohan", 10, "yohan@example.com", "DEVELOPER");
+        Administrator yohan2 = new Administrator(1, "yohan", 16, "yohan@other.com", "ANALYST");
 
         System.out.println("---");
         System.out.println(yohan);                // Member(1, yohan)
@@ -19,7 +19,7 @@ public class DemoApplication {
 
         System.out.println("---");
         System.out.println(yohan == yohan2);      // false
-        System.out.println(yohan.equals(yohan2)); // false -> true 로 바뀜.
+        System.out.println(yohan.equals(yohan2)); // true : Administrator 에서 재정의 안 돼서, Member의 equals 사용
     }
 
 }
