86f0876374d9a393b92e82824feef62d564bdc3f
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-21 14:16:57 +0900

    실습 2-26: @EqualsAndHashCode.Include + onlyExplicitlyIncluded 옵션 : 비교할 필드

--- src/main/java/com/example/demo/Member.java
+++ src/main/java/com/example/demo/Member.java
@@ -13,13 +13,15 @@ import lombok.ToString;
 @Getter // Class 단위
 @Setter
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false) // -> super=Member(1, yohan)
-@EqualsAndHashCode(of = {"id", "name"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
 
     @ToString.Include(name = "memberId")
+    @EqualsAndHashCode.Include
     private Integer id;
     @ToString.Include(rank = -1)
+    @EqualsAndHashCode.Include
     private String name;
     private int age;
     private String email;
