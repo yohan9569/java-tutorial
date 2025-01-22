3eb1db33ba1043543b08697a23a61eda985cbc54
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-22 12:29:56 +0900

    실습 1-12: 전체 필드 생성자(@AllArgsConstructor) 통한 객체 생성

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,11 @@
 package com.example.demo;
 
 import com.example.demo.member.Member;
-import com.example.demo.member.MemberRequestDto;
 
 public class DemoApplication {
 
     public static void main(String[] args) {
-        MemberRequestDto dto = new MemberRequestDto("yohan", "yohan@example.com");
-        Member yohan = new Member(dto);
-
-        yohan.setEmail("yohan@example.com");
+        Member yohan = new Member(1, "yohan", 30, "yohan@example.com");
 
         System.out.println("---");
         System.out.println(yohan);
