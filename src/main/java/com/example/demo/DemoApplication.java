c6975923be1ef64038b9b312935d9e40d4986e10
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-01-24 21:59:23 +0900

    실습 1-16: 빌더(@builder) 통한 객체 생성 장점 - 필드 설정 시점/역할 분리 가능

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,30 @@
 package com.example.demo;
 
 import com.example.demo.member.Member;
+import java.util.Objects;
 
 public class DemoApplication {
 
     public static void main(String[] args) {
-        //기존: Member yohan = new Member(1, "yohan", 30, "yohan@example.com");
-        Member yohan = Member.builder()     // 설정값을 모아주는 MemberBuilder 객체 반환, 여기에 하나하나 설정 넣어 줌.
-            .name("yohan")                  // 순서가 바껴도 된다.
-            .id(1)
+        String requestName = "zohan";
+
+        // (1) 빌더 정의
+        // (2) 필드 설정 : (2.1) 공통 부분
+        Member.MemberBuilder builder = Member.builder()
             .age(30)
-            // .email("yohan@example.com")  // 없어도 된다. (null) 기본값으로 들어감.
-            .build();                       // 자 이제 Member 만들어줘!
+            .email("common@example.com");
+        // (2) 필드 설정 : (2.2) 다른 부분은 따로 설정
+        if (requestName.equals("yohan")) {
+            builder
+                .id(1)
+                .name(requestName);
+        } else if (requestName.equals("zohan")) {
+            builder
+                .id(2)
+                .name("zohan");
+        }
+        // 객체 생성
+        Member yohan = builder.build();
 
         System.out.println("---");
         System.out.println(yohan);
