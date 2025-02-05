19258bc213c14712ad5a6f43a59621317479e8bb
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-02-05 14:59:11 +0900

    실습 0-4: Checked Exception 처리방법 2.2) 받기 (서버 오류 미발생) - try-catch

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,18 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    static void connect(String id, String password) throws SQLException { // Add Exception to Method Signature.
-        if (id.equals("admin") && password.equals("1234")) {
-            System.out.println("- Database is connected successfully.");
-        } else {
-            //throw new RuntimeException("데이터베이스 접속 실패");
-            throw new SQLException("데이터베이스 접속 실패");
+    static void connect(String id, String password) {
+        try {
+            if (id.equals("admin") && password.equals("1234")) {
+                System.out.println("- Database is connected successfully.");
+            } else {
+                //throw new RuntimeException("데이터베이스 접속 실패");
+                throw new SQLException("데이터베이스 접속 실패");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws SQLException { // Add Exception to Method Signature.
+    public static void main(String[] args) {
         connect("admin", "1234"); // - Database is connected successfully.
         connect("edmin", "4321"); // Exception in thread "main" java.lang.RuntimeException: 데이터베이스 접속 실패 at ~ at ~
     }
-
 }
