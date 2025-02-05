8702bc0f40675267ed2d1886fb6cf8f794097365
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-02-05 14:56:01 +0900

    실습 0-3: Checked Exception 처리방법 2.1) 넘기기 (서버 오류 발생) + throws 키워드

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,16 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    static void connect(String id, String password) {
+    static void connect(String id, String password) throws SQLException { // Add Exception to Method Signature.
         if (id.equals("admin") && password.equals("1234")) {
             System.out.println("- Database is connected successfully.");
         } else {
             //throw new RuntimeException("데이터베이스 접속 실패");
-            throw new SQLException("데이터베이스 접속 실패"); // 실행이 안 된다.must be caught or declared to be thrown
+            throw new SQLException("데이터베이스 접속 실패");
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException { // Add Exception to Method Signature.
         connect("admin", "1234"); // - Database is connected successfully.
         connect("edmin", "4321"); // Exception in thread "main" java.lang.RuntimeException: 데이터베이스 접속 실패 at ~ at ~
     }
